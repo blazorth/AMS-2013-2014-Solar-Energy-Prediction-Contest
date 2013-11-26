001699fb4e7fff9ea7cb978df0c5cda6c310b8dc
blazorth <blazorthes@free.fr>
2013-11-26 17:18:48 +0100

    Update GenerateModel.R

--- GenerateModel.R
+++ GenerateModel.R
@@ -1,7 +1,7 @@
 library(gbm)			‘ Load the gbm package
 
-DataTrain = read.table(“DataTrain.csv", header = T, sep = ",", dec = ".")		‘ Load training set
-DataTest = read.table(“DataTest.csv", header = T, sep = ",", dec = ".")		‘ Load testing set
+DataTrain = read.table("DataTrain.csv", header = T, sep = ",", dec = ".")		' Load training set
+DataTest = read.table("DataTest.csv", header = T, sep = ",", dec = ".")		  ' Load testing set
 
 Model.gbm <- gbm(Power ~ .,distribution="laplace", data=DataTrain, n.trees=3000, interaction.depth =6, shrinkage=0.05, bag.fraction)		‘ Train the model
 
